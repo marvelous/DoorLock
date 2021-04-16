67b6f18cac369a4329bb6d54070c9d1bbf86fc38
Maxime BORGES <maxime@hedtechnologies.com>
2021-04-16 22:23:31 +0200

    Add cool blinking LED and some interesting logs

--- DoorLock.ino
+++ DoorLock.ino
@@ -1,14 +1,18 @@
-#include "LDAP_lib.hpp"
+#include "ptldap/LDAP_lib.hpp"
 
 #include <sstream>
 #include <ESP8266WiFi.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include <FastLED.h>
 
 #define RST_PIN D4
-#define SS_PIN D2
+#define SS_PIN D8
 #define RELAY_PIN D1
 MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+#define NUM_LEDS 1
+CRGB leds[NUM_LEDS];
 
 // This file is in .gitignore
 // It should contain the following values:
@@ -40,28 +44,46 @@ void setup() {
   // Wait a bit, can help when resetting or reflashing some times
   delay(1000);
 
+  FastLED.addLeds<WS2812, D3, GRB>(leds, NUM_LEDS);
+  leds[0] = CRGB::Purple;
+  FastLED.show();
+
   // Init the SPI for the RFID reader
   SPI.begin();
 	mfrc522.PCD_Init();
+  mfrc522.PCD_DumpVersionToSerial();
   pinMode(RELAY_PIN, OUTPUT);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
+  Serial.print("Connecting to WiFi...");
+  bool led_state = false;
   while (WiFi.status() != WL_CONNECTED) {
+    if (led_state) {
+      leds[0] = CRGB::Purple;
+      FastLED.show();
+    } else {
+      leds[0] = CRGB::Black;
+      FastLED.show();
+    }
     delay(500);
     Serial.print(".");
+    led_state = !led_state;
   }
-
   Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+
+  Serial.print("WiFi connected, IP: ");
   Serial.println(WiFi.localIP());
 }
 
 void loop() {
+  leds[0] = CRGB::Red;
+  FastLED.show();
+
   // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
 	if (!mfrc522.PICC_IsNewCardPresent()) {
+    delay(100);
 		return;
 	}
 
@@ -69,18 +91,23 @@ void loop() {
 	if (!mfrc522.PICC_ReadCardSerial()) {
 		return;
 	}
-  
-  uint32_t badgenuid = 0;
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-    badgenuid += mfrc522.uid.uidByte[i] << ((mfrc522.uid.size - i - 1) * 8);
-	}
-  // Convert the int to string
+
+  leds[0] = CRGB::Blue;
+  FastLED.show();
+
+  // Save the badge NUID into a string
+  Serial.print("Badge NUID: ");
   string badgenuidstr;
   ostringstream badgenuidss;
-  badgenuidss << badgenuid;
+  for (char i = 0; i < mfrc522.uid.size; i++) {
+    if (mfrc522.uid.uidByte[i] < 0x10) {
+      Serial.print('0');
+    }
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    badgenuidss << (char) mfrc522.uid.uidByte[i];
+	}
+  Serial.println();
   badgenuidstr = badgenuidss.str();
-  Serial.print("Badge NUID: ");
-  Serial.println(badgenuidstr.c_str());
 
   // Connect to the LDAP server
   Serial.print("connecting to ");
@@ -101,6 +128,14 @@ void loop() {
   Serial.println("Connecting to LDAP");
   if (client.connected()) {
     auto req = LDAP::BindRequest(ldap_login, ldap_passwd).str();
+    Serial.println("> BindRequest");
+    for(int i = 0; i < req.length(); i++) {
+      if (req.c_str()[i] < 0x10) {
+        Serial.print('0');
+      }
+      Serial.print(req.c_str()[i], HEX);
+    }
+    Serial.println();
     client.write((const uint8_t*)req.c_str(), req.length());
   } else {
     client.stop();
@@ -119,8 +154,12 @@ void loop() {
   }
 
   // TODO: check if connection is accepted
+  Serial.println("<BindResponse");
   while (client.available()) {
     char ch = static_cast<char>(client.read());
+    if(ch < 0x10) {
+      Serial.print('0');
+    }
     Serial.print(ch, HEX);
   }
   Serial.println();
@@ -133,10 +172,19 @@ void loop() {
                                    "badgenuid", 
                                    badgenuidstr,
                                    "cn").str();
+    Serial.println(">SearchRequest");
+    for(int i = 0; i < req.length(); i++) {
+      if (req.c_str()[i] < 0x10) {
+        Serial.print('0');
+      }
+      Serial.print(req.c_str()[i], HEX);
+    }
+    Serial.println();
     client.write((const uint8_t*)req.c_str(), req.length());
   } else {
     client.stop();
     delay(2000);
+    Serial.println("Failed");
     return;
   }
 
@@ -151,9 +199,13 @@ void loop() {
 
   // TODO: properly check if an user is found
   string res;
+  Serial.println("<SearchResponse");
   while (client.available()) {
     char ch = static_cast<char>(client.read());
     res += ch;
+    if(ch < 0x10) {
+      Serial.print('0');
+    }
     Serial.print(ch, HEX);
   }
   Serial.println();
@@ -162,11 +214,24 @@ void loop() {
   client.stop();
 
   if (res.length() > 40) {
+    leds[0] = CRGB::Green;
+    FastLED.show();
+    Serial.println("Unlocking");
     digitalWrite(RELAY_PIN, HIGH);
     delay(2000);
     digitalWrite(RELAY_PIN, LOW);
+    leds[0] = CRGB::Red;
+    FastLED.show();
+    Serial.println("Locking back");
   } else {
-    delay(5000);
+    for(int i = 0; i < 5; i++) {
+      leds[0] = CRGB::Red;
+      FastLED.show();
+      delay(200);
+      leds[0] = CRGB::Black;
+      FastLED.show();
+      delay(200);
+    }
   }
 
   delay(1000);
